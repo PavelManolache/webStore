cddd12922a8c52d1a37293f2de8466bc4e57039d
PavelManolache <manolachepavelmihai@gmail.com>
2025-01-25 22:35:27 +0100

    Added vuetify for vite and tested it , it works

--- webStore/src/main.js
+++ webStore/src/main.js
@@ -3,9 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+const vuetify = createVuetify()
 
 const app = createApp(App)
-
+app.use(vuetify)
 app.use(router)
 
 app.mount('#app')
